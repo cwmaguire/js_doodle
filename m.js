d9735a3f0127b9dbc2daf57d054070f3370806ff
Chris Maguire <cwmaguire@gmail.com>
2016-01-13 00:05:22 -0800

    Slowly approaching a smooth wave
    Each consecutive dot can have more of it's angle applied so that the
    first dot's angle doesn't shake the whole line violently. If we apply a
    tiny bit of the first dot, a little bit more of the 2nd, etc. then each
    consecutive dots angle will come out more and more as it moves down the
    line, _hopefully_ producing a wave.

---
+++ m.js
@@ -0,0 +1,144 @@
+var go = true;
+var c;
+var h;
+var w;
+var mid;
+var MAX = 80;
+var dist = 5;
+
+out("t1", "50");
+init();
+
+function init(){
+  var c = document.getElementById("canvas1");
+  c.style.left = "310px";
+  c.style.top = "20px";
+
+}
+
+function animate(){
+  var c = document.getElementById("canvas1");
+  var ctx = c.getContext("2d");
+  var h = c.height = 500;
+  var w = c.width = 500;
+  ctx.translate(w / 2, h / 2);
+  animate_(ctx, []);
+}
+
+function animate_(ctx, objs){
+  var c = document.getElementById("canvas1");
+  var h = c.height;
+  var w = c.width;
+
+  var newObjs = updateObjects(objs);
+
+  var period = read("t1", "int");
+  var time = (new Date()).getTime();
+  out("t2", "Rendering at " + time);
+  render(ctx, {h: h, w: w}, newObjs.slice(0));
+  out("t3", "Finished at " + (new Date()).getTime());
+  var nextAnimateTime = time + period - (new Date()).getTime();
+
+  if(go){
+    setTimeout(function(){animate_(ctx, newObjs)}, nextAnimateTime);
+  }
+}
+
+function updateObjects(objs){
+  var newestObjs;
+  var last;
+  if(objs.length < MAX){
+    out("t7", "objs.length < MAX: " + objs.length);
+    newestObjs = objs.slice(0);
+  }else{
+    out("t7", "objs.length !< MAX: " + objs.length);
+    newestObjs = objs.slice(1);
+  }
+  if(objs.length == 0){
+    last = {x: 0, y: 0, a: 0};
+  }else{
+    last = newestObjs[newestObjs.length - 1];
+  }
+
+  //var randAngle = Math.random() * Math.PI / 15 - (Math.PI / 30);
+  var randAngle = Math.random() * Math.PI / 4;
+  out("t5", "randAngle: " + randAngle);
+  out("t6", "last: " + last);
+  //var dx = Math.floor(dist * Math.sin(randAngle));
+  //var dy = Math.floor(dist * Math.cos(randAngle));
+  var dx = dist;
+  var dy = dist;
+  out("t4", "Adding: " + dx + "," + dy);
+  newestObjs.push({x: dx, y: dy, a: randAngle});
+  return newestObjs.slice(0);
+}
+
+/*
+ * Trying to figure out why drawing a white rectangle won't clear out the
+ * screen.
+ *
+ * Ah! I wasn't using "beginPath()"
+ */
+
+function render(ctx, c, objs){
+  var x = clear(ctx, c);
+  //ctx.rotate(Math.PI / 64);
+  //ctx.strokeStyle = "#00F";
+  //ctx.beginPath();
+  //ctx.moveTo(0, 0);
+  //ctx.lineTo(200, 200);
+  //ctx.stroke();
+  //ctx.fillRect(50, 50, 30, 30);
+  //ctx.fillRect(10, 50, 30, 30);
+  //ctx.fillRect(50, 10, 30, 30);
+  //ctx.fillRect(10, 10, 30, 30);
+  var i = 0;
+  var prev;
+  var obj;
+  var totalX = 0;
+  var totalY = 0;
+  var totalA = 0;
+  var dx;
+  var dy;
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  for(i = 0; i < objs.length; i++){
+  //for(i = objs.length - 1; i >= 0; i--){
+    obj = objs[i];
+    out("t9", "obj.a: " + obj.a);
+    totalA += (obj.a * 4 / (objs.length * 2 - i));
+    out("t8", "totalA: " + totalA);
+    //dx = Math.floor(obj.x * Math.sin(obj.a));
+    //dy = Math.floor(obj.y * Math.cos(obj.a));
+    dx = Math.floor(obj.x * Math.sin(totalA));
+    dy = Math.floor(obj.y * Math.cos(totalA));
+    //totalX += obj.x;
+    //totalY += obj.y;
+    totalX += dx;
+    totalY += dy;
+    //ctx.arc(obj.x, obj.y, 2, 0, Math.PI * 2);
+    ctx.lineTo(totalX, totalY);
+  }
+  ctx.stroke();
+}
+
+function clear(ctx, c){
+  var left = -(c.w / 2);
+  var top = -(c.h / 2);
+  ctx.clearRect(left, top, c.w + 200, c.h + 200);
+}
+
+function out(field, text){
+  document.getElementById(field).value = text;
+}
+
+function read(field, type){
+  return cast(document.getElementById(field).value, type);
+}
+
+function cast(value, type){
+  switch(type){
+    case "int":
+      return parseInt(value);
+  }
+}
