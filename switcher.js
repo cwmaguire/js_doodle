0e8539601c7ee40dc452b6c1308bd32799506f9a
Chris Maguire <cwmaguire@gmail.com>
2018-12-19 00:12:20 -0800

    Add a script switcher
    One of the painful things about this project was having to edit the
    a.html file to try out different scripts. This adds a datalist input
    field for selecting which script to run. When a script is selected the
    animate function is called. Unfortunately I only started using the
    my animate script later in the project so I need to retrofit the old
    files (e.g. j.js) to work with the new script switcher. I _think_
    whatever new script is loaded will overwrite any existing functions that
    collide but I want to keep the animate() function from my animate
    JavaScript script.

---
+++ switcher.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const scripts = ["b.js",
+                 "c.js",
+                 "d.js",
+                 "e.js",
+                 "f.js",
+                 "g.js",
+                 "h.js",
+                 "i.js",
+                 "j.js",
+                 "k.js",
+                 "m.js",
+                 "n.js",
+                 "o.js",
+                 "p.js"];
+
+let currentScript;
+let isAnimationCancelled = false;
+
+function load_scripts(){
+  let datalist = elem('scripts');
+  for(let script of scripts){
+    let option = document.createElement("OPTION");
+    option.id = datalist.id + "_" + script;
+    option.value = script;
+    datalist.appendChild(option);
+  }
+}
+
+function change_script(event){
+  let input = event.target;
+  let value = input.value;
+  input.value = "";
+  let isAnimationCancelled = true;
+  let existingScript = currentScript;
+  if(currentScript){
+    currentScript.remove();
+  }
+  load_script(value);
+}
+
+function animation_continue_check(id){
+  console.log(`animation_continue_check(${id})`);
+  return !isAnimationCancelled;
+}
+
+function load_script(script){
+  let s = document.createElement("script");
+  s.src = script;
+  s.id = "script_" + script;
+  currentScript = s;
+
+  s.onload = function() {
+    animate(animation_continue_check);
+  };
+
+  document.body.appendChild(s);
+}
