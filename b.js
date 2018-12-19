96e41f76a0f450e181ef7fc7e7009b351ae88273
Chris Maguire <cwmaguire@gmail.com>
2018-12-19 00:07:39 -0800

    Switch from var to let
    Apparently let can now be used in every place where var is used to give
    better scoping and hoisting.
    
    https://hacks.mozilla.org/2015/07/es6-in-depth-let-and-const/

--- b.js
+++ b.js
@@ -4,8 +4,8 @@ function go(){
 }
 
 function b(){
-  var c = document.getElementById("canvas1");
-  var ctx = c.getContext("2d");
+  let c = document.getElementById("canvas1");
+  let ctx = c.getContext("2d");
   ctx.fillStyle = "#FFFFA0";
   ctx.strokeRect(0,0,c.width,c.height);
 }
@@ -15,37 +15,37 @@ function animate(times){
     time = (new Date()).getTime();
     times = [{rgb: timeRGB(time), time: time, rotations: 0}];
   }
-  var date = new Date();
-  var time = (new Date()).getTime();
-  var numRotates = draw_time(times);
-  var nextAnimateTime = time + 50 - (new Date()).getTime();
+  let date = new Date();
+  let time = (new Date()).getTime();
+  let numRotates = draw_time(times);
+  let nextAnimateTime = time + 50 - (new Date()).getTime();
   //console.log("Writing out at " + nextAnimateTime);
   console.log("Drawing took (" + (new Date()).getTime() + ", " + time + ") " + ((new Date()).getTime() - time));
   setTimeout(function(){animate(times)}, nextAnimateTime);
 }
 
 function draw_time(times){
-  var c = document.getElementById("canvas1");
-  var ctx = c.getContext("2d");
-  var time = (new Date()).getTime();
-  //var x = Math.floor((time % c.width) / 2);
-  //var y = Math.floor((time % c.height) / 2);
-  //var x = 10;
-  //var y = 10;
-  var x = Math.floor(c.width / 2);
-  var y = Math.floor(c.height / 2);
-  //var r = time % 255;
-  //var g = (time >> 3) % 255;
-  //var b = (time >> 6) % 255;
-  //var rgb = ("#" + r.toString(16) + g.toString(16) + b.toString(16)).toUpperCase();
-  var rgb = timeRGB(time);
+  let c = document.getElementById("canvas1");
+  let ctx = c.getContext("2d");
+  let time = (new Date()).getTime();
+  //let x = Math.floor((time % c.width) / 2);
+  //let y = Math.floor((time % c.height) / 2);
+  //let x = 10;
+  //let y = 10;
+  let x = Math.floor(c.width / 2);
+  let y = Math.floor(c.height / 2);
+  //let r = time % 255;
+  //let g = (time >> 3) % 255;
+  //let b = (time >> 6) % 255;
+  //let rgb = ("#" + r.toString(16) + g.toString(16) + b.toString(16)).toUpperCase();
+  let rgb = timeRGB(time);
   //console.log("Writing out " + time + " at " + x + ", " + y + " with: " + rgb + ")");
   //ctx.fillStyle = "#FF0000";
   if(times.length >= 16){
     times.shift();
   }
   //times = times.slice(1,7);
-  var rotations = times[0].rotations;
+  let rotations = times[0].rotations;
   times.push({rgb: rgb, time: time, rotations: (times[times.length - 1].rotations + 1) % 64});
 
   ctx.fillStyle = "#FFFFFF";
@@ -53,7 +53,7 @@ function draw_time(times){
 
   //console.log("Rotations: " + rotations);
 
-  var i = 0;
+  let i = 0;
 
   ctx.fillStyle = "#FFFFFF";
   ctx.strokeStyle = "#FFFFFF";
@@ -112,15 +112,15 @@ function draw_time(times){
 }
 
 function timeRGB(time){
-  var r = time % 255;
-  var g = (time >> 3) % 255;
-  var b = (time >> 6) % 255;
+  let r = time % 255;
+  let g = (time >> 3) % 255;
+  let b = (time >> 6) % 255;
   return ("#" + r.toString(16) + g.toString(16) + b.toString(16)).toUpperCase();
 }
 
 function drawLine(){
-  var c = document.getElementById("canvas1");
-  var ctx = c.getContext("2d");
+  let c = document.getElementById("canvas1");
+  let ctx = c.getContext("2d");
 
   ctx.fillStyle = "#000000";
   ctx.strokeStyle = "#000000";
@@ -132,8 +132,8 @@ function drawLine(){
 }
 
 function skew(){
-  var c = document.getElementById("canvas1");
-  var ctx = c.getContext("2d");
+  let c = document.getElementById("canvas1");
+  let ctx = c.getContext("2d");
 
   ctx.fillStyle = "#000000";
   ctx.strokeStyle = "#000000";
@@ -146,10 +146,10 @@ function skew(){
 }
 
 function rotate(){
-  var c = document.getElementById("canvas1");
-  var ctx = c.getContext("2d");
-  var x = c.width;
-  var y = c.height;
+  let c = document.getElementById("canvas1");
+  let ctx = c.getContext("2d");
+  let x = c.width;
+  let y = c.height;
 
   ctx.fillStyle = "#000000";
   ctx.strokeStyle = "#000000";
