e7c16842eca06a0b7a40f457b5c5a1825c91b1d2
Chris Maguire <cwmaguire@gmail.com>
2018-12-19 16:59:08 -0800

    Save and reset canvas props
    (At least) one of the scripts resets the positions of the canvases on
    the pages so now their states get saved and they get reset whenever a
    script is run.

--- a.js
+++ a.js
@@ -1,5 +1,54 @@
 "use strict";
 
+let jsDoodle = {
+  canvasProps: ['height', 'width'],
+  canvasStyleProps: ['top', 'left', 'height', 'width', 'border'],
+  canvases: [{id: 'canvas1', canvas: {style: {}}},
+             {id: 'canvas2', canvas: {style: {}}}],
+
+  save_canvas_states:
+    function(){
+      for(let c of jsDoodle.canvases){
+        jsDoodle.save_canvas_state(c);
+      }
+    },
+
+  save_canvas_state:
+    function ({id, canvas}){
+      let c = elem(id);
+
+      jsDoodle.canvasStyle = {top: c.style.top,
+                              left: c.style.left,
+                              height: c.style.height,
+                              width: c.style.width,
+                              border: c.style.border};
+      for(let p of jsDoodle.canvasProps){
+        canvas[p] = c[p];
+      }
+      for(let p of jsDoodle.canvasStyleProps){
+        canvas.style[p] = c.style[p];
+      }
+    },
+
+  reset_canvases:
+    function(){
+      for(let c of jsDoodle.canvases){
+        jsDoodle.reset_canvas(c);
+      }
+    },
+
+  reset_canvas:
+    function ({id, canvas}){
+      let c = elem(id);
+      for(let p of jsDoodle.canvasProps){
+        c[p] = canvas[p];
+      }
+      for(let p of jsDoodle.canvasStyleProps){
+        c.style[p] = canvas.style[p];
+      }
+    }
+}
+
 function out(field, text){
   document.getElementById(field).value = text;
 }
