d08ef9e2dffb08b4104e277a015bf71efb76c107
Chris Maguire <cwmaguire@gmail.com>
2018-12-27 23:51:10 -0800

    More work on "fireworks" ...
    - cause trailing circles to actual trail
    - reverse the direction (fall down, not up)
    - fix dimming (wrong field name: base_colours)
    - add some debugging to animate the fireworks age and radius
    - increase the frame count to track firework age
    - custom dimming amount (with default)
    - repeatedly decrease trail radius

--- r.js
+++ r.js
@@ -20,10 +20,10 @@ function init(){
 }
 
 function render({context: ctx, state: {h, w, frame, fireworks}}){
-  let maxFireworks = 100;
-  let minFramesBetweenFireworks = 30;
-  let maxFireworkAge = minFramesBetweenFireworks * 3;
-  let dy = 5;
+  let maxFireworks = 60;
+  let minFramesBetweenFireworks = 1;
+  let maxFireworkAge = 60;
+  let dy = 2;
 
   // we create falling fireworks that are bright at the head then fade to the tail.
   // The entire firework fades after a bit.
@@ -36,11 +36,14 @@ function render({context: ctx, state: {h, w, frame, fireworks}}){
   ctx.fillRect(0, 0, w, h);
 
   if(fireworks.length > 0){
-    console.log('firework one: ' + fireworks[0].base_color);
+    //console.log('firework one: ' + fireworks[0].base_color);
+  }
+
+  let isNotBlack = function({base_color}){
+    return !is_black(base_color);
   }
   // filter out black fireworks
-  fireworks = fireworks.filter(({base_color}) => !is_black(base_color));
-  console.log('fireworks length is ' + fireworks.length);
+  fireworks = fireworks.filter(isNotBlack);
 
   // Every X frames create a new firework if we don't have enough
   if(fireworks.length < maxFireworks && frame % minFramesBetweenFireworks == 0){
@@ -51,55 +54,80 @@ function render({context: ctx, state: {h, w, frame, fireworks}}){
   // Modify every firework to add a tail of diminishing brightness
   for(let firework of fireworks){
     let age = frame - firework.frame;
+    //console.log(`age: ${age}`);
+    //console.log(`maxFireworkAge: ${maxFireworkAge}`);
     let [r, g, b] = firework.base_color;
     if(age > maxFireworkAge){
-      [r, g, b] = dim_colors([r, g, b]);
-      firework.base_colors = [r, g, b];
+      [r, g, b] = dim_colors([r, g, b], 20);
+      //console.log(`r: ${r}, g: ${g}, b: ${b}`);
+      firework.base_color = [r, g, b];
     }
     let trail = create_trail(r, g, b);
     firework.trail = trail;
-    firework.y -= dy;
-    render_firework(ctx, firework);
+    firework.y += dy;
+    render_firework(ctx, firework, age);
   }
 
-  return {h: h, w: w, frame: frame, fireworks: fireworks};
+  return {h: h, w: w, frame: frame + 1, fireworks: fireworks};
 }
 
 function create_firework(h, w, frame){
   let x = Math.random() * w;
-  let y = Math.random() * (h - 40);
+  let y = Math.random() * (h - 200);
   let r = 255 - Math.floor(Math.random() * 50);
   let g = 255 - Math.floor(Math.random() * 50);
   let b = 255 - Math.floor(Math.random() * 50);
   return {x: x, y: y, base_color: [r, g, b], frame: frame};
 }
 
-function dim_colors([r, g, b]){
-  let dimAmount = 5;
+function dim_colors([r, g, b], dimAmount = 5){
   return [Math.max(0, r - dimAmount),
           Math.max(0, g - dimAmount),
           Math.max(0, b - dimAmount)]
 }
 
 function create_trail(r, g, b){
-  let dimAmount = 5;
+  let dimAmount = 20;
   let trail = [[r, g, b]];
   while(r > 0 || g > 0 || b > 0){
-    [r, g, b] = dim_colors([r, g, b]);
+    [r, g, b] = dim_colors([r, g, b], dimAmount);
     trail.push([r, g, b]);
   }
   return trail;
 }
 
-function render_firework(ctx, {trail, y, x}){
+function render_firework(ctx, {trail, y, x}, age = ''){
+  let radius = 5.0;
+  let effectiveRadius;
+  let dy = 5;
+  let y2 = y;
+  let rgbColor;
   for(let colors of trail){
+    radius = round(radius - 0.1, 3);
+    effectiveRadius = round(Math.max(1, Math.floor(radius)), 3);
+    y2 -= dy;
+    //rgbColor = rgb_color(colors);
+    //console.log(`rgbColor: ${rgbColor}`);
     ctx.fillStyle = rgb_color(colors);
     ctx.beginPath();
-    ctx.ellipse(x, y, 5, 5, 0, 0, Math.PI * 2);
-    ctx.stroke();
+    ctx.ellipse(x, y2, effectiveRadius, effectiveRadius, 0, 0, Math.PI * 2);
+    ctx.fill();
   }
+  //ctx.fillStyle = 'white';
+  ctx.fillStyle = rgb_color(trail[0]);
+  //console.log(`fillStyle: ${ctx.fillStyle}`);
+  //ctx.font = '12px serif';
+  //ctx.fillText('' + age, x + 10, y2)
+  //ctx.fillText('' + radius, x + 10, y2 + 12)
+  //ctx.fillText('' + effectiveRadius, x + 10, y2 + 24)
 }
 
 function is_black([r, g, b]){
   return r == 0 && g == 0 && b == 0;
+}
+
+function round(number, decimalPlaces){
+  let multiplier = Math.pow(10, decimalPlaces);
+  let rounded = Math.round(multiplier * number) / multiplier;
+  return rounded;
 }
