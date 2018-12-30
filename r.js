cb94843bb192fcbab4090331a9735bfc596237c8
Chris Maguire <cwmaguire@gmail.com>
2018-12-29 23:06:16 -0800

    Add more animation controls, show current value

--- r.js
+++ r.js
@@ -1,7 +1,8 @@
 "use strict";
 
 let MIN_HEIGHT = 100;
-let RADIUS = 5;
+let RADIUS = 10;
+let RADIUS_STEP = '0.1';
 let DIM = 10;
 let RED_BASE = 255;
 let GREEN_BASE = 255;
@@ -16,6 +17,8 @@ let GRADIENT_COLOR_STOP_1 = 0;
 let GRADIENT_COLOR_STOP_2 = 0.5;
 let GRADIENT_X_OFFSET = 0;
 let GRADIENT_Y_OFFSET = 0;
+let DELTA_Y = 2;
+let DELTA_Y_TRAIL = 5;
 
 function scriptDesc(){
   return 'falling fireworks';
@@ -42,7 +45,7 @@ function render({context: ctx, state: {h, w, frame, fireworks}}){
   let maxFireworks = get_control_value('max_fireworks', 'int');;
   let minFramesBetweenFireworks = 0;
   let maxFireworkAge = get_control_value('max_age', 'int');
-  let dy = 2;
+  let dy = get_control_value('delta_y', 'int');
 
   // we create falling fireworks that are bright at the head then fade to the tail.
   // The entire firework fades after a bit.
@@ -92,8 +95,8 @@ function render({context: ctx, state: {h, w, frame, fireworks}}){
 }
 
 function create_firework(h, w, frame){
-  let x = Math.random() * w;
-  let y = Math.random() * (h - get_control_value('min_height', 'int'));
+  let x = Math.floor(Math.random() * w);
+  let y = Math.floor(Math.random() * (h - get_control_value('min_height', 'int')));
   let redBase = get_control_value('red_base', 'int');
   let greenBase = get_control_value('green_base', 'int');
   let blueBase = get_control_value('blue_base', 'int');
@@ -119,33 +122,41 @@ function dim_colors([r, g, b], dimAmount = 5){
 
 function create_trail(r, g, b, age){
   let dimAmount = get_control_value('dim', 'int');
-  let trail = [[r, g, b]];
+  //let trail = [[r, g, b]];
+  let head = [r, g, b];
+  let trail = [];
   let maxTrail = Math.floor(age / 3);
   let i = 0;
   while(i < maxTrail && (r > 0 || g > 0 || b > 0)){
     [r, g, b] = dim_colors([r, g, b], dimAmount);
+    //trail.unshift([r, g, b]);
     trail.push([r, g, b]);
     i++;
   }
+  // Draw this last so it's not covered by the tail
+  trail.push(head);
   return trail;
 }
 
+// TODO flip the direction of the tail if dy is negative
+// (i.e. if we want the fireworks to go up)
 function render_firework(ctx, {trail, y, x}, age = ''){
-  let radius = get_control_value('radius', 'int');
-  let effectiveRadius;
-  let dy = 5;
-  let y2 = y;
+  let initialRadius = get_control_value('radius', 'int');
+  let dy = get_control_value('delta_y_trail', 'int');
+  let y2 = y - ((trail.length - 1) * dy);
   let rgbColor;
+  let radiusStep = get_control_value('radius_step', 'float');
+  let effectiveRadius = initialRadius - ((trail.length - 1) * radiusStep);
+  let radius;
 
   // Set the fill style and draw a rectangle
   ctx.fillStyle = gradient;
   //ctx.fillRect(20, 20, 160, 160);
 
-
   for(let colors of trail){
-    radius = round(radius - 0.1, 3);
-    effectiveRadius = round(Math.max(1, Math.floor(radius)), 3);
-    y2 -= dy;
+    effectiveRadius = round(effectiveRadius + radiusStep, 2);
+    radius = Math.max(1, Math.floor(effectiveRadius));
+    y2 += dy;
     //rgbColor = rgb_color(colors);
     //console.log(`rgbColor: ${rgbColor}`);
     //ctx.fillStyle = rgb_color(colors);
@@ -158,20 +169,25 @@ function render_firework(ctx, {trail, y, x}, age = ''){
       gradientRadius1,
       x,
       y2,
-      effectiveRadius);
+      radius);
     // I think color stops specify where the transition begins and ends
     let colorStop1 = get_control_value('gradient_color_stop_1', 'float');
     let colorStop2 = get_control_value('gradient_color_stop_2', 'float');
     gradient.addColorStop(colorStop1, rgb_color(colors));
-    gradient.addColorStop(colorStop2, 'black');
+    gradient.addColorStop(colorStop2, 'transparent');
     ctx.fillStyle = gradient;
 
     ctx.beginPath();
     ctx.ellipse(x, y2, effectiveRadius, effectiveRadius, 0, 0, Math.PI * 2);
     ctx.fill();
+
+    // render the location of the gradient fill
+    //ctx.fillStyle = rgb_color(colors);
+    //ctx.font = '12px serif';
+    //ctx.fillText('x: ' + x + ', y2: ' + y2, x + 30, y2)
   }
   //ctx.fillStyle = 'white';
-  ctx.fillStyle = rgb_color(trail[0]);
+  //ctx.fillStyle = rgb_color(trail[0]);
   //console.log(`fillStyle: ${ctx.fillStyle}`);
   //ctx.font = '12px serif';
   //ctx.fillText('' + age, x + 10, y2)
@@ -197,7 +213,8 @@ function get_control_value(controlName, type){
 
 function add_controls(w, h){
   add_slider('min_height', '0', h, '10', MIN_HEIGHT);
-  add_slider('radius', '0', w, '1', RADIUS);
+  add_slider('radius', '0', Math.floor(w / 4), '1', RADIUS);
+  add_slider('radius_step', '0.0', '0.9', '0.1', RADIUS_STEP);
   add_slider('dim', '0', '255', '1', DIM);
   add_slider('red_base', '0', '255', '1', RED_BASE);
   add_slider('green_base', '0', '255', '1', GREEN_BASE);
@@ -212,6 +229,8 @@ function add_controls(w, h){
   add_slider('gradient_color_stop_2', '0.0', '1.0', '.05', GRADIENT_COLOR_STOP_2);
   add_slider('gradient_x_offset', '-50', '50', '1', GRADIENT_X_OFFSET);
   add_slider('gradient_y_offset', '-50', '50', '1', GRADIENT_Y_OFFSET);
+  add_slider('delta_y', '-10', '10', '1', DELTA_Y);
+  add_slider('delta_y_trail', '-20', '20', '1', DELTA_Y_TRAIL);
 }
 
 function add_slider(name, min, max, step, value){
@@ -230,7 +249,19 @@ function add_slider(name, min, max, step, value){
   label.innerText = name;
   label.for = name;
 
+  let text = document.createElement('INPUT');
+  text.type = 'text';
+  text.id = name + '_text';
+  text.name = name + '_text';
+  let sliderOnchange =
+    function(event){
+      console.log(`event: ${event}`);
+      text.value = event.target.value;
+    };
+  slider.addEventListener('change', sliderOnchange);
+
   controlSpan.appendChild(slider);
   controlSpan.appendChild(label);
+  controlSpan.appendChild(text);
   controlSpan.appendChild(document.createElement('BR'));
 }
