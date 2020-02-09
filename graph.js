3a3af9a1574cfa25732d04d1753dab433109ed04
Chris Maguire <cwmaguire@gmail.com>
2020-02-09 00:28:48 -0800

    WIP: walk graph with back-tracking
    My idea now is for each vertex then return to it's predecessor with any
    siblings it has. The predecessor will then make sure all of the siblings
    that are directly connected are next to each other. Once all of the
    siblings for a vertex are drawn, then each sibling is drawn. Each
    sibling will connect itself to any directly connected siblings.
    
    Example:
    Given the following graph
    
    .   .
     \ /
      1-----2
       \   /
        \ /
         3
         |
         |
         4
    
    Assuming we start from 3,
    Calculate position of 3,
    Call calculate with vertex 1 but passing in:
    - a record of vertex 3
    - the starting angle (zero)
    Vertex 1 calculates it's position
    Vertex 1 returns that it has a connection to 2
    Call calculate with the sibling that was connected to 1, i.e. 2
      Pass in:
    - the record of vertex 3
    - the connection to 2
    - the next angle (i.e. Math.PI / 3
    Now 1 and 3 are both connected to 3 and each other and are beside each
    other.
    Now draw 4.
    Once four is done call calculate on 1 _again_;
      Pass in that 3 is finished so 1 can recurse
      ... probably pass in the angle of 1 relative to 3 to continue in that
    direction
    ...
    
    Anyways, that's the idea.

--- graph.js
+++ graph.js
@@ -14,10 +14,13 @@ let EDGE_LENGTH = 100;
  *
  */
 
-let GRAPH = {1: {'edges': [2, 3, 4]},
-             2: {'edges': []},
-             3: {'edges': [1, 4]},
-             4: {'edges': [1, 3]}}
+
+const GRAPH = {1: [2, 3, 4, 6],
+               2: [1, 3],
+               3: [1, 2],
+               4: [1, 5],
+               5: [4, 6],
+               6: [1, 5]};
 
 function scriptDesc(){
   return 'Draw a graph';
@@ -32,324 +35,123 @@ function init(){
   let ctx = c.getContext('2d');
   let h = c.height;
   let w = c.width;
-  let verteces = [];
-  let edges = [];
+  let state = undefined;
 
   add_controls(w, h);
 
-  //let firstIndex = 0;
-  let x = 200;
-  let y = 200;
-  let nullStartingAngle = undefined;
-  let firstVertex = vertex_shape('1', x, y);
-  let initialShapes = [firstVertex];
-  let firstId = parseInt(Object.keys(GRAPH)[0]);
-  let verteces =
-    [{id: firstVertex,
-      x: x,
-      y: y,
-      arc: Math.PI * 2,
-      arc_start: 0}];
-  let uniqueEdges = unique_edges(GRAPH);
+  shapes = arrange_shapes(GRAPH, c.width, c.height);
 
-  const state = {graph: GRAPH,
-                 shapes: initialShapes,
-                 verteces: verteces};
-  let arrangement = maybe_arrange_shapes(state);
 
-  const shapes = arrangement.arranged_shapes;
+  // return state used for rendering
+  return state;
+}
 
-  return {h: h, w: w, frame: 1, shapes: shapes};
+function arrange_shapes(graph, w, h){
+  let state = {parent: undefined,
+               angle: undefined,
+               remaining: {},
+               connections: {},
+               locations: {},
+               w: w,
+               h: h}
+
+  let firstKey = Object.keys(graph)[0];
+  arrange_vector(graph, state, firstKey);
+}
+
+function arrange_vector(graph, state, key){
+  // draw myself
+  point = location(state);
+
+  // record my connections
+
+  // if I have an unfinished parent return new state
+
+  // else, if I have any unarranged siblings, arrange them.
+  // For each one, if they return a connected sibling, draw
+  // that sibling next and tell it to connect to the previous
+  // sibling
+  //
+  // I can probably just look through the connections and if I find
+  // one between me and something else then I can draw it.
+  // The connections will be populated by siblings when they return
+  // after drawing just themselves.
+
+}
+
+function location({angle: angle, locations: locations}){
+  let point;
+  if(state.angle == undefined){
+    x = state.w / 2;
+    y = state.y / 2;
+    point = {x: x, y: y}
+  }else{
+    parentLocation = state.locations[state.parent]
+    point = calculate_point(state.angle, parentLocation);
+  }
+
+  return point;
+}
+
+function calculate_point(angle, start){
+  let x, y, point
+  if(angle == 0 || angle == Math.PI * 2){
+    point = {x: start.x + EDGE_LENGTH, y: start.y}
+  if(angle < (Math.PI / 2)){
+    point = quad_1_point(angle, start);
+  }else if(angle == Math.PI / 2){
+    point = {x: start.x, y: start.y - EDGE_LENGTH}
+  }else if(angle > (Math.PI / 2) && angle < Math.PI){
+    point = quad_2_point(angle, start);
+  }else if(angle == Math.PI){
+    point = {x: start.x - EDGE_LENGTH, y: start.y}
+  }else if(angle > Math.PI && angle < (3 / 2 * Math.PI)){
+    point = quad_3_point(angle, start);
+  }else if(angle == (3 / 2 * Math.PI)){
+    point = {x: start.x, y: start.y + EDGE_LENGTH}
+  }else if(angle > (3 / 2 * Math.PI) && angle < (2 * Math.PI)){
+    point = quad_4_point(angle, start);
+  }
+  return point;
+}
+
+function quad_1_point(angle, start){
+  xd = Math.cos(angle) * EDGE_LENGTH;
+  x = start.x + xd;
+  yd = Math.sin(angle) * EDGE_LENGTH;
+  y = start.y - yd;
+  return {x: x, y: y};
+}
+
+function quad_2_point(angle, start){
+  xd = Math.cos(Math.PI - angle) * EDGE_LENGTH;
+  x = start.x - xd;
+  yd = Math.sin(Math.PI - angle) * EDGE_LENGTH;
+  y = start.y - yd;
+  return {x: x, y: y};
+}
+
+function quad_3_point(angle, start){
+  xd = Math.cos(angle - Math.PI) * EDGE_LENGTH;
+  x = start.x - xd;
+  yd = Math.sin(angle - Math.PI) * EDGE_LENGTH;
+  y = start.y + yd;
+  return {x: x, y: y};
+}
+
+function quad_4_point(angle, start){
+  xd = Math.cos(Math.PI - angle) * EDGE_LENGTH;
+  x = start.x + xd;
+  yd = Math.sin(Math.PI - angle) * EDGE_LENGTH;
+  y = start.y + yd;
+  return {x: x, y: y};
 }
 
 function render({context: ctx, state: {h, w, frame, shapes}}){
   for(const shape of shapes){
-    let debug = 'shape:';
-    for(const property in shape){
-      debug += ` ${property}: ${shape[property]}`
-    }
-    //console.log(debug);
     draw_shape(ctx, shape);
   }
   return {h: h, w: w, frame: frame, shapes: shapes};
-}
-
-function maybe_graph_to_shapes(state){
-
-  if(is_graph_arranged(state)){
-    return {arranged_shapes: state.shapes.slice(0),
-            arranged_edge_ids: new Set(ids_by_type(state.shapes, 'edge')),
-            arranged_vertex_ids: new Set(ids_by_type(state.shapes, 'vertex'))};
-  }
-
-  graph_to_shapes(state);
-}
-
-// find all the verteces that are connected to the incoming list of verteces
-// sort verteces by loops
-// create a vertex shape for each vertex
-// create an edge shape for each vertex connecting back to previous shell
-// create edge shapes between verteces in same shell
-function graph_to_shapes(state){
-
-  // Each incoming vertex has an arc I can divy up that arc for each of that
-  // vertex' "outer" siblings
-  //
-  // ... and I have a LIST of these verteces, each with its own ark
-
-  //const isShape = (id) => state.shapes.filter(({id: id_}) => id == id_).length > 0;
-
-  const verteces_ = state.verteces.map(verteces);
-
-
-
-  const siblingIds = outer_sibling_ids(state).filter((id) => !isShape(id));
-  const shapes = vertex_shapes(state, siblingIds);
-
-  const getId = ({id: id}) => id;
-  const getSiblings = (id) => GRAPH[id].edges
-  const verteces =  state.verteces.map(getId).flatMap(getSiblings);
-
-  const id = state.id;
-  const siblingIds = unique_siblings(state.graph, id);
-  const edgeIds = edge_ids(id, siblingIds);
-  //const unarrangedEdges = set_minus(edgeIds, arrangedEdgeIds);
-  const numEdges = edgeIds.length;
-  let dAngle = 0;
-  const hasEdgesToArrange = numEdges > 0;
-
-  log_arrange_shapes(id, siblingIds, edgeIds, hasEdgesToArrange);
-
-  if(hasEdgesToArrange){
-    dAngle = 2 * Math.PI / (numEdges);
-  }
-
-  const numArrangedEdges = numEdges - unarrangedEdges.size;
-
-  let _arrangedEdgeIds = new Set(arrangedEdgeIds);
-  let _arrangedVertexIds = new Set(arrangedVertexIds);
-  const paddingAngle = numArrangedEdges * dAngle;
-
-  for(let i = 0; i < siblingIds.length; i++){
-    if(angle === undefined){
-      angle = 0;
-    }
-    const siblingId = siblingIds[i];
-    console.log(`Arranging sibling vertex ${siblingId}`);
-    const edgeId = edge_id(previousId, siblingId);
-    const adjustedDeltaAngle = (dAngle * i)
-    const tau = (Math.PI * 2);
-    const newAngle = (angle + paddingAngle + adjustedDeltaAngle) % tau;
-
-    var edgeShape;
-
-    //const isEdgeArranged = _arrangedEdgeIds.has(edgeId);
-    //console.log(`is edge ${edgeId} arranged? ${isEdgeArranged}`);
-
-    //const isVertexArranged = has_key(verteces, siblingId)
-    //console.log(`is vertex ${siblingId} arranged? ${isVertexArranged}`);
-
-    //if(isVertexArranged){
-    //edgeShape = edge_between_verteces_shape();
-    //}else{
-    edgeShape = edge_at_angle_shape(edgeId, previousX, previousY, newAngle);
-    //}
-
-    //if(isEdgeArranged && isVertexArranged){
-      //continue;
-    //}
-
-    //if(!isEdgeArranged){
-    newArrangedShapes.push(edgeShape);
-    _arrangedEdgeIds.add(edgeId);
-    }
-
-    const vertexShape = vertex_shape(siblingId, edgeShape.x2, edgeShape.y2);
-    //if(!isVertexArranged){
-    newArrangedShapes.push(vertexShape);
-    _arrangedVertexIds.add(siblingId);
-    //}
-
-  }
-
-  let edges = edges(uniqueEdges, siblingIds);
-
-  for(let i = 0; i < siblingIds.length; i++){
-    
-  }
-
-  for(let i = 0; i < siblingIds.length; i++){
-    const arrangedSubGraph =
-      arrange_shapes(graph,
-                     arrangedShapes,
-                     uniqueEdges,
-                     siblingId,
-                     _arrangedEdgeIds,
-                     _arrangedVertexIds,
-                     vertexShape.x,
-                     vertexShape.y,
-                     newAngle);
-
-    newArrangedShapes = newArrangedShapes.concat(arrangedSubGraph.arranged_shapes);
-    _arrangedEdgeIds = set_union(_arrangedEdgeIds, arrangedSubGraph.arranged_edge_ids);
-    _arrangedVertexIds = set_union(_arrangedVertexIds, arrangedSubGraph.arranged_vertex_ids);
-  }
-
-  return {arranged_shapes: newArrangedShapes,
-          arranged_edge_ids: new Set(arrangedEdgeIds),
-          arranged_vertex_ids: new Set(arrangedVertexIds)}
-}
-
-// what do I get:
-//   a vertex
-// what do I return:
-//   that vertex plus its outer verteces, plus their outer verteces, etc.
-// This will necessarily give me [v1, [v2, [v4], v3, [v5]]] ... which I could just flatten
-//
-// The terminal case is when a vertex doesn't have any outer siblings,
-// then we just return itself as a list [v]
-// Then the previous caller on the stack can flatten all lists and add itself to the
-// beginning (unshift)
-function verteces(state, vertex){
-  // get the verteces for this
-  const vertexIds = get_outer_vertexes(state, vertex);
-
-  // calculate the arc for each vertex
-  const arc = vertex.arc * vertexIds.length;
-  const arcAccumulator = {arc: arc, start: vertex.arc_start};
-  const {verteces: verteces_} = vertexIds.reduce(vertex_with_arc, arcAccumulator);
-
-  // call verteces on each of those
-  const outerVerteces = vertexIds.flatMap(verteces_);
-
-  // unshift vertex onto the front
-  outerVerteces.unshift(vertex);
-
-  // return the resulting list.
-  return outerVertces.slice(0);
-
- 
-}
-
-function vertex_with_arc(id, {verteces: verteces, arc: arc, arc_start: arcStart}){
-  const newVertex = {id: id, x: x, y: y, arc: arc, arc_start: arc_start};
-  const newVerteces = verteces.slice(0);
-  newVertces.unshift(newVertex);
-  return {verteces: newVerteces, arc: arc, arc_start: arcStart + arc};
-}
-
-function log_arrange_shapes(id, siblingIds, edgeIds, arrangedEdgeIds, hasEdgesToArrange){
-
-  console.log(`Arranging vertex ${id} with siblings ${siblingIds}`);
-  console.log(`Arranging vertex ${id}, has edges to arrange? ${hasEdgesToArrange}`);
-
-  for(const edgeId of edgeIds){
-    console.log(`Arrange vertex ${id}, has edge ${edgeId}`);
-  }
-
-  for(const edgeId of arrangedEdgeIds){
-    console.log(`Arrange vertex ${id}, already arranged edge ${edgeId}`);
-  }
-}
-
-function is_graph_arranged(state){
-  const uniqueEdges = unique_edges(state.graph);
-  const numUniqueEdges = state.unique_edges.size;
-  const numUniqueVerteces = (new Set(Object.keys(state.graph))).size;
-  const areAllEdgesArranged = ids_by_type(state.shapes, 'edge').length >= numUniqueEdges;
-  const areAllVertecesArranged = ids_by_type(state.shapes, 'vertex').length >= numUniqueVerteces;
-  return areAllEdgesArranged && areAllVertecesArranged;
-}
-
-function has_key(dict, key){
-  return (new Set(Object.keys(verteces))).has(key);
-}
-
-function vertex_shape(id, x, y){
-  return {type: 'vertex',
-          id: id,
-          x: x,
-          y: y};
-}
-
-function edge_shape(id, x, y, angle){
-  let edgeLength = get_control_value('edge_length', 'int');
-  let dx = Math.cos(angle) * edgeLength;
-  let dy = Math.sin(angle) * edgeLength;
-  console.log(`edge_shape: id: ${id}, edgeLength: ${edgeLength}, x1: ${x}, y1: ${y}, dx: ${dx}, dy: ${dy}, x2: ${x + dx}, y2: ${y + dy}`);
-  return {type: 'edge',
-          id: id,
-          x1: x,
-          y1: y,
-          x2: x + dx,
-          y2: y + dy};
-}
-
-function edge_ids(fromId, toIds){
-  let edgeIds = [];
-  for(let toId of toIds){
-    edgeIds.push(edge_id(fromId, toId));
-  }
-  return edgeIds;
-}
-
-function edge_id(vertexId1, vertexId2){
-  const minVertexId = Math.min(vertexId1, vertexId2);
-  const maxVertexId = Math.max(vertexId1, vertexId2);
-  return `${minVertexId}-${maxVertexId}`;
-}
-
-function vertex_edges(uniqueEdges, vertexIds){
-  let siblingPairs = vertex_pairs(vertexIds);
-}
-
-function ids_by_type(shapes, type){
-  const isEdge = ({type: t}) => t == type;
-  const getId = ({id: id}) => id;
-  return shapes.filter(idEdge).map(getId);
-}
-
-function next_pairs(graph, id, arc, arcStart){
-  const getId = ({id: id}) => id;
-  const siblingPairs =
-    (id) =>
-      sibling_pairs(id, arc, arcStart);
-  return state.verteces.map(getId).map(getSiblingIds).flatMap(create_pairs);
-}
-
-function sibling_pairs(id, arc, arcStart){
-  const siblings = GRAPH[id].edges
-  const newSiblings = 
-  const angle = arc / siblings
-
-}
-
-function sibling_pairs(vertexIds){
-  return something;
-}
-
-function unique_edges(graph){
-  let uniqueEdges = new Set([]);
-  for(const vertex in graph){
-    for(const edge of graph[vertex]['edges']){
-      const vertex1 = Math.min(vertex, edge);
-      const vertex2 = Math.max(vertex, edge);
-      uniqueEdges.add('' + vertex1 + '-' + vertex2);
-    }
-  }
-  return uniqueEdges;
-}
-
-function angles({
-
-function is_black([r, g, b]){
-  return r == 0 && g == 0 && b == 0;
-}
-
-function round(number, decimalPlaces){
-  let multiplier = Math.pow(10, decimalPlaces);
-  let rounded = Math.round(multiplier * number) / multiplier;
-  return rounded;
 }
 
 function add_controls(w, h){
